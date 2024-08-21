e4bad2b6fa3f7b9d72fc955c26d1f1bdb96f3fac
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-08-21 16:22:14 -0400

    Create initialization statements in classes

--- game.rb
+++ game.rb
@@ -3,3 +3,6 @@ class Game
     @board = Board.new
     @players = [Player.new("Player 1", "X"), Player.new("Player 2", "O")]
     @current_player = @players.first
+  end
+
+    
---
+++ main.rb
@@ -0,0 +1,8 @@
+require './player.rb'
+require './board.rb'
+require './game.rb'
+
+def play_game
+  game = Game.new
+  # Start game
+end
