0aab2f63aeea90ecf1b0bf00210a774f590459ad
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-08-22 16:03:21 -0400

    Build Game class

--- game.rb
+++ game.rb
@@ -5,4 +5,31 @@ class Game
     @current_player = @players.first
   end
 
+  def play
+    loop do
+      @board.display
+      move = @current_player.get_move
+      if @board.valid_move?(move)
+        @board.update(move, @current_player.marker)
+        if @board.winner?
+          @board.display
+          puts "#{@current_player.name} wins!"
+          break
+        elsif @board.full?
+          @board.display
+          puts "It's a tie!"
+          break
+        else
+          switch_turns
+        end
+      else
+        puts "Invalid move. Please try again."
+      end
+    end
+  end
+
+  def switch_turns
+    @current_player = @players.rotate!.first
+  end
+end
     
