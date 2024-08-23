94fea36a2704891522c4495a374a7f9aa389eee6
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-08-23 15:27:39 -0400

    Add win detection to Board class

--- board.rb
+++ board.rb
@@ -21,4 +21,25 @@ class Board
     (0..2).include?(row) && (0..2).include?(col) && @grid[row][col] == " "
   end
 
+  def full?
+    @grid.flatten.none? { |cell| cell == " " }
+  end
+
+  def winner?
+    winning_combinations.any? do |combo|
+      markers = combo.map { |row, col| @grid[row][col] }
+      markers.uniq.length == 1 && markers.first != " "
+    end
+  end
+
+  def winning_combinations
+    rows = @grid
+    cols = @grid.transpose
+    diagonals = [
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]]
+    ]
+    rows + cols + diagonals
+  end
+
 end
